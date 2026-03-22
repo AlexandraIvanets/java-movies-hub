e8008629d49d9dbaf3dbf5f91d5a6305f8f7c86f
Aleksandra Ivanets <ivanets.alexa@yandex.ru>
2026-03-22 17:15:41 +0300

    bugfix: checkstyle Endpoint

--- src/main/ru/practicum/moviehub/http/Endpoint.java
+++ src/main/ru/practicum/moviehub/http/Endpoint.java
@@ -1,3 +1,10 @@
 package ru.practicum.moviehub.http;
 
-enum Endpoint {GET_MOVIES, POST_MOVIES, GET_MOVIE_BY_ID, DELETE_MOVIE_BY_ID, GET_MOVIES_BY_YEAR, UNKNOWN}
+enum Endpoint {
+    GET_MOVIES,
+    POST_MOVIES,
+    GET_MOVIE_BY_ID,
+    DELETE_MOVIE_BY_ID,
+    GET_MOVIES_BY_YEAR,
+    UNKNOWN
+}
